7bb336c9c1d3e727bf34b843c441ff1866ef1002
luke <305867377@qq.com>
2024-09-01 16:29:43 +1000

    displaying the genres 008-20

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,20 @@ import { GenreList } from './components/GenreList'
 function App() {
   return <Grid templateAreas={{
     base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"` //1024px
-  }}>
+    lg: `"nav nav" "aside main"`, //1024px
+    
+  }}
+  templateColumns= {{
+    base: '1fr',
+    lg: '300px 1fr'
+  }}
+  
+  >
     <GridItem area='nav'>
       <NavBar/>
     </GridItem>
     <Show above='lg'>
-      <GridItem area='aside' >
+      <GridItem area='aside' paddingX={5}>
         <GenreList/>
       </GridItem>
     </Show>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,17 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
 import useGenres from '../hookers/useGenres'
+import getCroppedImageUrl from '../hookers/image-url'
 
 export const GenreList = () => {
     const {data} = useGenres()
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
+                <HStack> 
+                    <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>                 
+                    <Text fontSize='lg'>{genre.name}</Text>
+                </HStack>
+                </ListItem>)}
+        </List>
     )
     }
