7a968c8a92e8ba8529f937f644a3e918265f9b35
luke <305867377@qq.com>
2024-12-14 15:23:55 +1100

    004-21 Improving the Layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import useGame from '../hookers/useGame'
-import { Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react'
+import { Box, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react'
 import ExpandableText from '../components/ExpandableText'
 import GameAttributes from '../components/GameAttributes'
 import GameTrailer from '../components/GameTrailer'
@@ -15,13 +15,20 @@ const GameDetailPage = () => {
   if(error || !game) throw error;
 
   return (
-    <>
+    <SimpleGrid columns={{base: 1, md: 2}} spacing={5}>
+    <Box >
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game}/>
+      </Box>
+      <Box>
+
       <GameTrailer gameId={game.id} />
       <GameScreenShots gameId={game.id} />
-    </>
+
+      </Box>
+
+    </SimpleGrid>
   )
 }
 
